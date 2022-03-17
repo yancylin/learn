ace336f3e7c308e6a48dbee390364c3016f50b6b
姚贞桢 <yaozz@syn-trust.net>
2022-03-17 09:43:54 +0800

    feature:改革

--- .umirc.ts
+++ .umirc.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'learn',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: '/images/logo.png',
+  logo: '/images/logo.png',
   outputPath: 'docs-dist',
   mode: 'site',
-  // base: '/learn',
-  // publicPath: '/learn/',
+  base: '/learn',
+  publicPath: '/learn/',
   // exportStatic: {
   //   htmlSuffix: false,
   //   dynamicRoot: false,
